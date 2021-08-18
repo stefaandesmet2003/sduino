7e3e81c7dfd04e7147e6e46fef5fd47828820498
Stefaan De Smet <stefaandesmet2003@yahoo.co.uk>
2021-08-18 14:14:48 +0200

    quick&dirty port of AVR Keypad library

---
+++ hardware/stm8/0.5.0/libraries/Keypad/examples/EventKeypad/EventKeypad.ino
@@ -0,0 +1,78 @@
+/* @file EventSerialKeypad.pde
+ || @version 1.0
+ || @author Alexander Brevig
+ || @contact alexanderbrevig@gmail.com
+ ||
+ || @description
+ || | Demonstrates using the KeypadEvent.
+ || #
+ */
+#include <Keypad_stm8.h>
+
+#define ROWS 4   //four rows
+#define COLS 3   //three columns
+//STM8 const byte ROWS = 4; //four rows
+//STM8 const byte COLS = 3; //three columns
+char keys[ROWS][COLS] = {
+    {'1','2','3'},
+    {'4','5','6'},
+    {'7','8','9'},
+    {'*','0','#'}
+};
+
+byte rowPins[ROWS] = {5, 4, 3, 2}; //connect to the row pinouts of the keypad
+byte colPins[COLS] = {8, 7, 6}; //connect to the column pinouts of the keypad
+
+#define ledPin LED_BUILTIN
+
+boolean blink = false;
+boolean ledPin_state;
+
+void keypadEvent(KeypadEvent key);
+
+void setup(){
+    Serial_begin(115200);
+    pinMode(ledPin, OUTPUT);              // Sets the digital pin as output.
+    digitalWrite(ledPin, HIGH);           // Turn the LED on.
+    ledPin_state = digitalRead(ledPin);   // Store initial LED state. HIGH when LED is on.
+
+    Keypad_init (makeKeymap(keys), rowPins, colPins, ROWS, COLS);
+    Keypad_addEventListener(keypadEvent); // Add an event listener for this keypad
+}
+
+void loop(){
+    char key = Keypad_getKey();
+
+    if (key) {
+        Serial_println_c(key);
+    }
+    if (blink){
+        digitalWrite(ledPin,!digitalRead(ledPin));    // Change the ledPin from Hi2Lo or Lo2Hi.
+        delay(100);
+    }
+}
+
+// Taking care of some special events.
+void keypadEvent(KeypadEvent key){
+    switch (Keypad_getState()){
+    case PRESSED:
+        if (key == '#') {
+            digitalWrite(ledPin,!digitalRead(ledPin));
+            ledPin_state = digitalRead(ledPin);        // Remember LED state, lit or unlit.
+        }
+        break;
+
+    case RELEASED:
+        if (key == '*') {
+            digitalWrite(ledPin,ledPin_state);    // Restore LED state from before it started blinking.
+            blink = false;
+        }
+        break;
+
+    case HOLD:
+        if (key == '*') {
+            blink = true;    // Blink the LED when holding the * key.
+        }
+        break;
+    }
+}
